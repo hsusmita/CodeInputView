d8abab0f8eb38f5c904fb486972a36dfa751cd54
hsusmita <susmita.horrow@gmail.com>
2015-06-04 15:42:06 +0530

    fixedLengthTextField added

---
+++ CodeInputView/Source/FixedLengthTextField.h
@@ -0,0 +1,27 @@
+//
+//  FixedLengthTextField.h
+//  CodeInputView
+//
+//  Created by hsusmita on 04/06/15.
+//  Copyright (c) 2015 hsusmita.com. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@protocol FixedLengthTextFieldDelegate;
+
+@interface FixedLengthTextField : UITextField  <UIKeyInput>
+
+@property (nonatomic,assign) NSInteger maximumCharacterLimit;
+@property (nonatomic,weak) id<FixedLengthTextFieldDelegate> fixedLengthTextFieldDelegate;
+
+@end
+
+@protocol FixedLengthTextFieldDelegate <NSObject>
+
+- (void)didDeleteFromEmptyTextField:(FixedLengthTextField *)textField;
+- (void)didInsertToFullTextField:(FixedLengthTextField *)textField;
+- (void)textLimitReachedForTextField:(FixedLengthTextField *)textField;
+
+@end
+
---
+++ CodeInputView/Source/FixedLengthTextField.m
@@ -0,0 +1,47 @@
+//
+//  FixedLengthTextField.m
+//  CodeInputView
+//
+//  Created by hsusmita on 04/06/15.
+//  Copyright (c) 2015 hsusmita.com. All rights reserved.
+//
+
+#import "FixedLengthTextField.h"
+
+@implementation FixedLengthTextField
+
+- (void)deleteBackward {
+  [super deleteBackward];
+  if (self.text.length == 0) {
+    if ([self.fixedLengthTextFieldDelegate respondsToSelector:@selector(didDeleteFromEmptyTextField:)]) {
+      [self.fixedLengthTextFieldDelegate didDeleteFromEmptyTextField:self];
+    }
+  }
+}
+
+- (void)insertText:(NSString *)text {
+  [super insertText:text];
+  [self truncateCharactersIfRequired];
+}
+
+- (void)paste:(id)sender {
+  [super paste:sender];
+  [self truncateCharactersIfRequired];
+}
+
+- (void)truncateCharactersIfRequired {
+  if (self.text.length == self.maximumCharacterLimit) {
+    if ([self.fixedLengthTextFieldDelegate respondsToSelector:@selector(textLimitReachedForTextField:)]) {
+      [self.fixedLengthTextFieldDelegate textLimitReachedForTextField:self];
+    }
+  }else if (self.text.length > self.maximumCharacterLimit) {
+    NSString *string = [self.text substringWithRange:NSMakeRange(0, self.maximumCharacterLimit)];
+    self.text = string;
+    if ([self.fixedLengthTextFieldDelegate respondsToSelector:@selector(didInsertToFullTextField:)]) {
+      [self.fixedLengthTextFieldDelegate didInsertToFullTextField:self];
+    }
+  }
+}
+
+
+@end
