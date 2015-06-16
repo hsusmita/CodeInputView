f7a78c8f4678158a205dfb56a603750284901b3e
hsusmita <susmita.horrow@gmail.com>
2015-06-16 13:10:28 +0530

    Stepper to adjust height added

--- CodeInputView/Example/RoundedInputField.m
+++ CodeInputView/Example/RoundedInputField.m
@@ -18,8 +18,8 @@
 }
 */
 
-- (void)setFrame:(CGRect)frame {
-  [super setFrame:frame];
+- (void)layoutSubviews {
+  [super layoutSubviews];
   self.layer.cornerRadius = self.frame.size.width/2;
   self.layer.masksToBounds = YES;
 }
--- CodeInputView/ViewController.m
+++ CodeInputView/ViewController.m
@@ -13,6 +13,7 @@
 @interface ViewController ()
 
 @property (weak, nonatomic) IBOutlet CodeInputView *codeInputView;
+@property (weak, nonatomic) IBOutlet NSLayoutConstraint *heightConstraint;
 
 @end
 
@@ -21,13 +22,21 @@
 - (void)viewDidLoad {
   [super viewDidLoad];
   [self.codeInputView registerNibWithName:NSStringFromClass([RoundedInputField class])];
-  [self.codeInputView configureWithBoxCount:5 withBoxSize:CGSizeMake(60, 60)];
-//  [self.codeInputView configureWithBoxCount:4 withInset:UIEdgeInsetsMake(10, 10, 10, 10) withInterBoxSpace:10];
+//  [self.codeInputView configureWithBoxCount:5 withBoxSize:CGSizeMake(60, 60)];
+  [self.codeInputView configureWithBoxCount:5 withInset:UIEdgeInsetsMake(10, 10, 10, 10) withInterBoxSpace:20];
 }
 
 - (void)didReceiveMemoryWarning {
   [super didReceiveMemoryWarning];
   // Dispose of any resources that can be recreated.
+}
+- (IBAction)updateHeight:(id)sender {
+  
+}
+- (IBAction)adjustConstraint:(UIStepper*)stepper {
+  NSLog(@"value = %f",stepper.value);
+  self.heightConstraint.constant = stepper.value;
+  
 }
 
 @end
